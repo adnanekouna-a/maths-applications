5cff31de04e4d18e53eb586ea0774fe2c2a3de7c
Adnane Kouna <adnanekouna.a@gmail.com>
2020-10-22 19:59:00 +0100

    Operations now working

--- maths-functions.py
+++ maths-functions.py
@@ -1,14 +1,28 @@
 def multiply(x, y):
-    x*y
+    return x*y
 
 
 def add(x, y):
-    x+y
+    return x+y
 
 
 def substract(x, y):
-    x-y
+    return x-y
 
 
 def divide(x, y):
-    x/y
+    return x/y
+
+
+x = int(input('x = '))
+y = int(input('y = '))
+operation = input('Operation [A, S, M, D] : ')
+
+if operation == 'A':
+    print(add(x, y))
+elif operation == 'S':
+    print(substract(x, y))
+elif operation == 'M':
+    print(multiply(x, y))
+elif operation == 'D':
+    print(divide(x, y))
